984f795b4d00b4ef9f4e4301d807a0722dc8c6e4
Linkin143 <kumarprince143p143@gmail.com>
2024-12-15 17:39:09 +0530

    navbar style updated

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link, useSearchParams } from "react-router-dom";
 
 const Navbar = () => {
+  const [searchParams] = useSearchParams();
+  let todosDatahere = searchParams.get("todo");
+
   return (
     <nav>
-      <Link to="/" className="">All</Link>
-      <Link to="/?todo=active" className="">Active</Link>
-      <Link to="/?todo=completed" className="">Completed</Link>
+      <Link to="/" className={todosDatahere===null? "active" : ""}>All</Link>
+      <Link to="/?todo=active" className={todosDatahere==="active"? "active": ""}>Active</Link>
+      <Link to="/?todo=completed" className={todosDatahere==="complted"? "active":""}>Completed</Link>
      
     </nav>
   )
